d154fe1f3fcfb22ddfc70e6d371b6c189bc07733
cby <chenboyan@ict.ac.cn>
2021-01-05 21:46:08 +0800

    ci(GetConfig) config parser logic finish

--- Load_Balancer/GetConfig.cs
+++ Load_Balancer/GetConfig.cs
@@ -40,18 +40,16 @@ namespace HyperVWcfTransport
 
         public class SysConfig
         {
-            public string VMName { set; get; }
-            public bool Installed { set; get; }
-            public UInt64 MemorySize { set; get; }
-            public int CPUNum { set; get; }
-            public int CPULimt { set; get; }
-            public SysConfig(string _VMName, bool _Installed, UInt64 _MemorySize, int _CPUNum, int _CPULimit)
+            public string BalanceStrategy { set; get; }
+            public int CPUDefaultCount { set; get; }
+            public UInt64 MemoryDefaultSize { set; get; }
+            public int CPUDefaultLimit { set; get; }
+            public SysConfig(string _BalanceStrategy, int _CPUDefaultCount, UInt64 _MemoryDefaultSize, int _CPUDefaultLimit)
             {
-                VMName = _VMName;
-                Installed = _Installed;
-                MemorySize = _MemorySize;
-                CPUNum = _CPUNum;
-                CPULimt = _CPULimit;
+                BalanceStrategy = _BalanceStrategy;
+                CPUDefaultCount = _CPUDefaultCount;
+                MemoryDefaultSize = _MemoryDefaultSize;
+                CPUDefaultLimit = _CPUDefaultLimit;
             }
         }
 
@@ -83,6 +81,24 @@ namespace HyperVWcfTransport
             return currentVMConfig;
         }
 
-        public 
+        public SysConfig GetSysConfig()
+        {
+            using (System.IO.StreamReader file = System.IO.File.OpenText(SysConfigPath))
+            {
+                using (JsonTextReader reader = new JsonTextReader(file))
+                {
+                    JObject o = (JObject)JToken.ReadFrom(reader);
+                    if (o.ContainsKey("BalanceStrategy"))
+                        currentSysConfig.BalanceStrategy = o["BalanceStrategy"].ToString();
+                    if (o.ContainsKey("CPUDefaultCount"))
+                        currentSysConfig.CPUDefaultCount = Convert.ToInt32(o["CPUDefaultCount"].ToString());
+                    if (o.ContainsKey("MemoryDefaultSize"))
+                        currentSysConfig.MemoryDefaultSize = Convert.ToUInt64(o["MemoryDefaultSize"].ToString());
+                    if (o.ContainsKey("CPUDefaultLimit"))
+                        currentSysConfig.CPUDefaultLimit = Convert.ToInt32(o["CPUDefaultLimit"].ToString());
+                    return currentSysConfig;
+                }
+            }
+        }
     }
 }
